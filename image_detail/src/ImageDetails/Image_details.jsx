f81e2a989bf2ec7918a6341a8ebbfd390ab1de6e
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-22 12:47:45 +0330

    update and create div ...

--- image_detail/src/ImageDetails/Image_details.jsx
+++ image_detail/src/ImageDetails/Image_details.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
-
+import Basketball from './Basketball.jpg'
 const Image_details = () => {
   return (
     <div>
-        <h1>Starting ...</h1>
+        <div>
+            <div>
+                <img src={Basketball} alt="Shoe" width={80} height={60}/>
+            </div>
+            <div>
+                <h2>Basketball</h2>
+                <div className='size'>
+                    <h3>size</h3>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
