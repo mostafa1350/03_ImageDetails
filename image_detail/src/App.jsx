1a2e0da487a6bc69a2aa8ddca224574aebc5824e
Mostafa Hosseini <54173709+mostafa1350@users.noreply.github.com>
2024-12-22 09:26:40 +0330

    Create File and install tailwind

---
+++ image_detail/src/App.jsx
@@ -0,0 +1,11 @@
+import Image_details from './ImageDetails/Image_details'
+
+function App() {
+
+  return (
+    <>
+      <Image_details />
+    </>
+  )
+}
+export default App
---
+++ image_detail/src/ImageDetails/Image_details.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Image_details = () => {
+  return (
+    <div>
+        <h1>Starting ...</h1>
+    </div>
+  )
+}
+
+export default Image_details
